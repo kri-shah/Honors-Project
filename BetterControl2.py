0efcc03104c71163b6d47cae3e69730353a39712
kri-shah <114970376+kri-shah@users.noreply.github.com>
2022-12-05 18:57:04 -0500

    Update BetterControl2.py

--- BetterControl2.py
+++ BetterControl2.py
@@ -22,40 +22,45 @@ class gaming:
             self.answer_key.add(frozenset(sorted(temp_list)))  
             temp_list = [] 
         
-        
+        #creates a start list, shuffles to be fair
         self.start_list = random.sample(temp, 16)
-        self.pairs = set(combinations(self.start_list, 2))
+        
+        #makes all possible combinations
+        self.pairs = set(combinations(self.start_list, 2)) 
+        
+        #creates the start weeks and answer set
         self.weeks = 0
         self.my_ans = []
-        
 
     def run(self, pos_solutions = None):
+        #creates initial test case- just all pos solutions to begin with
         if pos_solutions is None:
-            pos_solutions = self.pairs
-        #print(f"{pos_solutions} \n\n")
-        if len(self.my_ans) == 8:
-            return self.weeks 
+            pos_solutions = self.pairs.copy()
+        #base case- if answer key has 8 pairs (so 16 people), returns weeks taken
+        if len(self.my_ans) == 8:            
+            return self.weeks
         
-        for pos in pos_solutions.copy():
+        #copies pos solutions to avoid set modify error- iterates through possible combinations
+        for x in pos_solutions.copy():            
+            #if pos solution is correct, adds to answer key
             self.weeks += 1
-            if frozenset(pos) in self.answer_key:
-                self.my_ans.append(pos)
-                t1 = pos[0]
-                t2 = pos[1]
-                for p in self.pairs.copy():
-                    if t1 in p:
-                        self.pairs.remove(p) 
-                    elif t2 in p:
-                        self.pairs.remove(p) 
-                if len(self.my_ans) < 5:
-                    self.run(self.pairs) 
-                else:
-                    print(f"my ans:{(self.my_ans)}   corr: {self.answer_key}")
-                    return self.weeks 
+            if set(x) in self.answer_key:
+                self.my_ans.append(x)
+                #scans through the pairs remaining and if either of the options are in a possible pair removes
+                #removes as it cannot be a possible combination if it is known to be correct with another person
+                for y in self.pairs.copy():
+                    if x[0] in y or x[1] in y:
+                        self.pairs.remove(y) 
+                #recursively calls funtion with newer shortened list
+                return self.run(self.pairs) 
+            else:
+                #can just 
+                self.pairs.remove(x) 
 
 
 
-n_trials = 1
+
+n_trials = 1000
 avg_weeks = 0
 avg_time = 0
 for x in range(n_trials):
@@ -71,4 +76,3 @@ for x in range(n_trials):
 
 print(f"Avg weeks is {avg_weeks/n_trials}, for {n_trials} trials")
 print(f"Avg time is {avg_time/n_trials}, for {n_trials} trials")
-
